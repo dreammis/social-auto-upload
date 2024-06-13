8b80580fc7c435e4786938b6b9e79d101db83d9b
pookz@stme <pookz@stme.com>
2024-06-13 18:13:21 +0800

    1. add cli support. 2. change LOCAL_CHROME_PATH to conf file 3. change douyin gen cookie url

---
+++ utils/base_social_media.py
@@ -0,0 +1,14 @@
+from typing import List
+
+SOCIAL_MEDIA_DOUYIN = "douyin"
+SOCIAL_MEDIA_TENCENT = "tencent"
+SOCIAL_MEDIA_TIKTOK = "tiktok"
+SOCIAL_MEDIA_BILIBILI = "bilibili"
+
+
+def get_supported_social_media() -> List[str]:
+    return [SOCIAL_MEDIA_DOUYIN, SOCIAL_MEDIA_TENCENT, SOCIAL_MEDIA_TIKTOK]
+
+
+def get_cli_action() -> List[str]:
+    return ["upload", "login", "watch"]
