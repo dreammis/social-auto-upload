106d7acc35b884ece536a69b017c486bb40a82a0
Edan's Win11 <961150665@qq.com>
2025-06-02 20:30:03 +0800

    feat: 新增 ai2video 方法

--- conf.py
+++ conf.py
@@ -2,4 +2,4 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).parent.resolve()
 XHS_SERVER = "http://127.0.0.1:11901"
-LOCAL_CHROME_PATH = ""   # change me necessary！ for example C:/Program Files/Google/Chrome/Application/chrome.exe
+LOCAL_CHROME_PATH = "C:\Program Files\Google\Chrome\Application\chrome.exe"   # change me necessary！ for example C:/Program Files/Google/Chrome/Application/chrome.exe
