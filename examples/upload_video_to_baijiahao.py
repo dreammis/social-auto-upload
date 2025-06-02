794e8feaea0304d2580f0598c40d7d8900e4feeb
Edan's Win11 <961150665@qq.com>
2025-06-02 20:30:03 +0800

    feat: 新增 ai2video 方法

--- examples/upload_video_to_baijiahao.py
+++ examples/upload_video_to_baijiahao.py
@@ -20,8 +20,8 @@ if __name__ == '__main__':
         title, tags = get_title_and_hashtags(str(file))
         thumbnail_path = file.with_suffix('.png')
         # 打印视频文件名、标题和 hashtag
-        print(f"视频文件名：{file}")
-        print(f"标题：{title}")
-        print(f"Hashtag：{tags}")
+        # print(f"视频文件名：{file}")
+        # print(f"标题：{title}")
+        # print(f"Hashtag：{tags}")
         app = BaiJiaHaoVideo(title, file, tags, publish_datetimes[index], account_file)
-        asyncio.run(app.main(), debug=False)
+        asyncio.run(app.mainAi(), debug=False)
