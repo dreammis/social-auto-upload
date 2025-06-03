b098ab69f77550c304092209e7537e81f6d5bdff
Edan's Win11 <961150665@qq.com>
2025-06-03 21:23:56 +0800

    fix: 修改第三方库的引入

--- sau_frontend/src/main.js
+++ sau_frontend/src/main.js
@@ -1,11 +1,20 @@
-//引入createApp用于创建应用
 import { createApp } from 'vue'
-//引入APP根组件
 import App from './App.vue'
+import router from './router'
+import pinia from './stores'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import './styles/index.scss'
 
 const app = createApp(App)
-import router from './router'
+
+// 注册 Element Plus 图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 app.use(router)
-
+app.use(pinia)
+app.use(ElementPlus)
 app.mount('#app')
