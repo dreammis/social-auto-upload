2c5fa0a9ff807e79a7b8b4cf0fd80de83ab5eb27
Edan's Win11 <961150665@qq.com>
2025-06-03 21:27:10 +0800

    feat: 新增 api 接口

---
+++ sau_frontend/src/api/account.js
@@ -0,0 +1,24 @@
+import { http } from '@/utils/request'
+
+// 账号管理相关API
+export const accountApi = {
+  // 获取有效账号列表
+  getValidAccounts() {
+    return http.get('/getValidAccounts')
+  },
+  
+  // 添加账号
+  addAccount(data) {
+    return http.post('/account', data)
+  },
+  
+  // 更新账号
+  updateAccount(id, data) {
+    return http.put(`/account/${id}`, data)
+  },
+  
+  // 删除账号
+  deleteAccount(id) {
+    return http.get(`/deleteAccount?id=${id}`)
+  }
+}
---
+++ sau_frontend/src/api/index.js
@@ -0,0 +1,9 @@
+// API 统一导出
+export * from './user'
+export * from './account'
+export * from './material'
+
+// 可以在这里添加其他API模块的导出
+// export * from './product'
+// export * from './order'
+// export * from './common'
