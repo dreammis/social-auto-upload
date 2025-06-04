61fbd42585d5aac075d29099f41393f4b190a7b8
Edan's Win11 <961150665@qq.com>
2025-06-04 10:42:19 +0800

    fix: 去掉 conf.py 文件，改为 example 示例

---
+++ conf.example.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent.resolve()
+XHS_SERVER = "http://127.0.0.1:11901"
+LOCAL_CHROME_PATH = ""   # change me necessary！ for example C:/Program Files/Google/Chrome/Application/chrome.exe
--- conf.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-BASE_DIR = Path(__file__).parent.resolve()
-XHS_SERVER = "http://127.0.0.1:11901"
-LOCAL_CHROME_PATH = "C:\Program Files\Google\Chrome\Application\chrome.exe"   # change me necessary！ for example C:/Program Files/Google/Chrome/Application/chrome.exe
